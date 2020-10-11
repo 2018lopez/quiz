892b92ce37c8ffb39c735ee34862d4ee3e061f11
codelopez501 <codelopez501@gmail>
2020-10-11 00:03:25 -0600

    Shell Script: Even/Odd

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+
+echo "----------------Even or Odd----------"
+
+echo "-------------------------------------"
+echo   "Enter a Number: "
+
+read number
+
+
+
+
+if [ $((number%2)) -eq 0 ]
+then 
+
+	echo "Number is even"
+
+else 
+	echo "Number is odd"
+fi
